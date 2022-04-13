b4024206db9e43cca1c4a5be8cc72e6899e6189b
Mario Aleo <mario.aleo@golsat.com.br>
2022-04-13 10:16:45 -0300

    chore: code format

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-restricted-globals */
 /* global importScripts, workbox */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js'
+);
 
 self.addEventListener('install', () => {
   self.skipWaiting();
